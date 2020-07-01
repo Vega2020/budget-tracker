8afc20ed426377644080d88dc332b18246d521bc
Jeremy Flowers <flowersje08@gmail.com>
2020-07-01 15:07:15 -0700

    attempting to get caches to load, can't find error in service worker/manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "/icons"];
+const FILES_TO_CACHE = ["/", "/index.js", "/icons/icon-192x192.png", "/icons/icon-512x512.png", "/icons/icon-144x144.png", "/manifest.webmanifest"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function(evt) {
+  console.log(evt);
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log("Your files were pre-cached successfully!");
